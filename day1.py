42b8b73fd0f13b20e0a68396333954925fdde82d
MSchick27 <mschick.stud@gmail.com>
2022-12-01 22:27:29 +0100

    day1

---
+++ day1.py
@@ -0,0 +1,21 @@
+#%%PART A
+data = open('day1_puzzleinput.txt','r')
+datalist= data.read().split('\n\n')
+
+clist = []
+for item in datalist:
+    itemlist = item.split('\n')
+    sumlist = []
+    for it in itemlist:
+        it = int(it)
+        sumlist.append(it)
+    cal = sum(sumlist)
+    clist.append(cal)
+
+
+clist.sort()
+print(clist[-1])
+
+#%%PART B
+print(sum(clist[-3:]))
+
