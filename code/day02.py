137c81cfe1cf906a8ed6d3abf6dd718272f01226
MSchick27 <mschick.stud@gmail.com>
2022-12-02 10:57:29 +0100

    day2sec commit

--- code/day02.py
+++ code/day02.py
@@ -2,13 +2,13 @@
 wiki={'A':1,'X':1,'B':2,'Y':2,'C':3,'Z':3}
 points={-1:6,2:6,0:3,1:0,-2:0}
 
-games = open('/Users/mschick/Desktop/Advent of code 2022/code/day02_puzzleinput.txt','r').readlines()
+games = open('day02_puzzleinput.txt','r').readlines()
 pts=[]
 for game in games:
 	plays = game.split(' ')
 	pts.append(points[wiki[plays[0].strip()]-wiki[plays[1].strip()]] + wiki[plays[1].strip()])
-
 print(sum(pts))
+
 
 
 
@@ -24,7 +24,6 @@ for game in games:
     arr = np.array(sorted([(wiki[plays[0].strip()] - item) for item in cmd[plays[1].strip()]]))
     yourplay = arr[(arr >= 1) & (arr <= 3)][0] #nice way to remove all upper and lower values arr[(arr >= lower) & (arr <= upper)] instead of np.clip()
     pp.append(yourplay + points[cmd[plays[1].strip()][0]])
-
 print(sum(pp))
 
 
